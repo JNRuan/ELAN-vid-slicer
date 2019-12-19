7ff1a75edc486f5b7937341e78eba075573e0b3c
JNRuan <j.nguyen@uqconnect.edu.au>
2019-12-19 14:02:53 +1000

    Version 0.1: Slices all annotations from video from specified input folder.

--- src/elan_video.py
+++ src/elan_video.py
@@ -4,10 +4,11 @@
 #
 # Makes extensive use of the Elan data structure provided by pympi library.
 #
+# Version: 0.1
 # Author: Jameson Nguyen (JNRuan), University of Queensland
-#
 ##############################################################################
 from pympi import Elan
+from typing import List, Tuple
 import os
 import sys
 
@@ -18,11 +19,14 @@ class ElanVideo:
 
     Data is extracted from ELAN (.eaf) files and their associated video file.
     """
-    def __init__(self, input_dir: str, elan_fname: str=None, tier_name: str=None, tier_index: int=1):
+    def __init__(self, input_dir: str,
+                 elan_fname: str=None,
+                 tier_name: str=None,
+                 tier_index: int=1):
         self._eaf = None
-        self._path = None
-        self._elan_fname = None
-        self._name = None
+        self.path = None
+        self.elan_fname = None
+        self.name = None
         self._load_elan(input_dir, elan_fname)
 
     @property
@@ -30,12 +34,23 @@ class ElanVideo:
         return self._eaf
 
     def _load_elan(self, input_dir: str, elan_fname: str=None):
+        """Loads ELAN file from provided input directory.
+        If no elan file name is given then will attempt to find an Elan (.eaf)
+        file in the folder. Should there be more than one Elan (.eaf) file then
+        the first one will be loaded unless a name is specified.
+
+        Retrieves file information and builds eaf file.
+
+        :param str input_dir: Folder provided by user that contains an Elan (.eaf) file.
+        :param str elan_fname: (Optional) Elan file name if provided by user,
+                                in format name.eaf.
+        """
         # Elan file provided, load directly.
         if elan_fname:
             if elan_fname.endswith('.eaf'):
-                self._path = os.path.join(input_dir, elan_fname)
+                self.path = os.path.join(input_dir, elan_fname)
             else:
-                self._path = os.path.join(input_dir, f'{elan_fname}.eaf')
+                self.path = os.path.join(input_dir, f'{elan_fname}.eaf')
         else:
             # No Elan file, load indirectly - inform user if not found or there is more than one.
             elan_file_list = [file for file in os.listdir(input_dir) if file.endswith('.eaf')]
@@ -46,29 +61,29 @@ class ElanVideo:
                         " by default using the first file in folder."
                         " Please specify the file you want to use with the optional",
                         " argument -e or --elan_file on script execution")
-                self._path = os.path.join(input_dir, elan_file_list[0])
+                self.path = os.path.join(input_dir, elan_file_list[0])
             else:
-                self._path = os.path.join(input_dir, elan_file_list[0])
+                self.path = os.path.join(input_dir, elan_file_list[0])
 
         try:
-            self._eaf = Elan.Eaf(self._path)
+            self._eaf = Elan.Eaf(self.path)
         except Exception as e:
-            print(f"Error on attempting to load Elan file from {self._path}")
+            print(f"Error on attempting to load Elan file from {self.path}")
             print(e)
 
         if self._eaf:
-            self._elan_fname = os.path.basename(self._path)
-            self._name = os.path.splitext(self._elan_fname)[0]
-            print(f"Loaded elan file: {self._elan_fname}")
+            self.elan_fname = os.path.basename(self.path)
+            self.name = os.path.splitext(self.elan_fname)[0]
+            print(f"Loaded elan file: {self.elan_fname}")
 
-    def get_tier_names(self):
+    def get_tier_names(self) -> List[str]:
         """Retrieves a list of all available tier names in tier index order.
 
         :returns: List of tier names (str).
         """
         return list(self._eaf.get_tier_names())
 
-    def get_tier_by_index(self, tier_index: int):
+    def get_tier_by_index(self, tier_index: int) -> str:
         """Returns tier name at tier index.
 
         Note this is zero-indexed, so tier index 1 should be passed as 0.
@@ -82,7 +97,7 @@ class ElanVideo:
             return None
         return self.get_tier_names()[tier_index]
 
-    def get_annotations_from_tier(self, tier: str):
+    def get_annotations_from_tier(self, tier: str) -> List[Tuple[int, int, str]]:
         """Retrieves all annotations from the provided tier.
 
         :param str tier: The tier name to retrieve from.
--- src/elan_video_slicer.py
+++ src/elan_video_slicer.py
@@ -2,15 +2,17 @@
 ##############################################################################
 # ELAN Video Slicer Util
 #
-# Main driver file for ELAN Video Slicer utility.
+# Main driver file for ELAN Video Slicer utility. Requires ffmpeg.
 #
 # Version: 0.1
 # Author: Jameson Nguyen (JNRuan), University of Queensland
 ##############################################################################
 # Libary Imports
 from datetime import timedelta
+from typing import List, Tuple
 import argparse
 import os
+import re
 import subprocess
 
 # Module Imports
@@ -23,10 +25,18 @@ class ElanVideoSlicer:
         self.elan_video = ElanVideo(args.input_dir, args.elan_file)
         self.tier_name = args.tier_name
         self.tier_index = args.tier_index
-        self.output_dir = args.output_dir
         self.fps = args.fps
+        # Make Output Folder, 'OutputFolder/fps_n'
+        self.output_path = os.path.join(args.output_dir, f"fps_{self.fps}")
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
 
-    def get_annotations(self):
+    def get_annotations(self) -> List[Tuple[int, int, str]]:
+        """Get all annotations from user provided tier name or tier index.
+
+        :returns: List of annotations with start and end times in milliseconds,
+                    at selected tier. (start, end, annotation)
+        """
         if self.tier_name:
             print(f"Retrieving annotations from tier name {self.tier_name}")
             return self.elan_video.get_annotations_from_tier(self.tier_name)
@@ -39,7 +49,42 @@ class ElanVideoSlicer:
         """Runs Elan Video Slicer: Calls ffmpeg on the cmd line so user will
         need to have ffmpeg installed.
         """
+        annotations = self.get_annotations()
+        for start_time, end_time, annotation in annotations:
+            t_start = str(timedelta(milliseconds=start_time))
+            t_end = str(timedelta(milliseconds=end_time))
+            label = annotation.lower()
+            label = re.sub(r'[\\/*?:"<>|]', "-", label)
+            outpath = os.path.join(self.output_path, label)
+            if not os.path.exists(outpath):
+                os.makedirs(outpath)
+            inpath = os.path.dirname(self.elan_video.path)
+            self._subprocess_call_ffmpeg(inpath, outpath,
+                                         t_start, t_end,
+                                         self.elan_video.name,
+                                         label,
+                                         self.fps)
 
+    def _subprocess_call_ffmpeg(self, inpath: str, outpath: str,
+                                start_time, end_time,
+                                id: str, annotation: str,
+                                fps: int):
+        """Calls ffmpeg subroutine.
+
+        Example:
+            ffmpeg -i id.mp4 -ss hh:mm:ss.ms -to hh:mm:ss.ms -vf fps=10 outpath/annotation/id_annotation_10fps_%04d.jpg
+        """
+        vid_path = os.path.join(inpath, f"{id}.mp4")
+        frame_name = f"{id}_{annotation}_{fps}fps_%04d.jpg"
+        frame_path = os.path.join(outpath, frame_name)
+        subprocess.call([
+            'ffmpeg',
+            '-i', vid_path,
+            '-ss', start_time,
+            '-to', end_time,
+            '-vf', f'fps={fps}',
+            frame_path
+        ], shell=True)
 
 def main():
     # Setup Parser
@@ -55,7 +100,7 @@ def main():
     # Optional Args
     parser.add_argument('-o', '--output_dir',
                         type=str,
-                        default='./output',
+                        default='../output',
                         help='''Output directory for parsed annotations with
                                 video frames. Each annotation will contain its own folder.''')
     parser.add_argument('-f', '--fps',
@@ -76,40 +121,10 @@ def main():
 
     args = parser.parse_args()
 
-    # Check Output
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-
     # Run App
     elan_vid_slicer = ElanVideoSlicer(args)
     if elan_vid_slicer.elan_video.eaf:
-        # TESTING
-        # annotations = elan_vid_slicer.get_annotations()
-        # print(annotations)
-        #
-        # # TIME TESTER
-        # start, end, annotation = annotations[0]
-        # start = str(timedelta(milliseconds=start))
-        # end = str(timedelta(milliseconds=end))
-        # print(f"{start} - {end}: {annotation}")
-        #
-        # fname = elan_vid_slicer.elan_video._elan_fname
-        # fname = os.path.splitext(fname)[0]
-        # vid_name = os.path.join(args.input_dir, f'{fname}.mp4')
-        # print(vid_name)
-        # fps = args.fps
-        # out = os.path.join(args.output_dir, annotation, f'{fname}_{annotation}_{fps}fps_%04d.jpg')
-        #
-        # subprocess.call(['ffmpeg',
-        #                 '-i', vid_name,
-        #                 '-ss', start,
-        #                 '-to', end,
-        #                 '-vf',
-        #                 f'fps={fps}',
-        #                 f'{fname}_{fps}fps_%04d.jpg'
-        #                 ], shell=True)
-
-        print("EAF LOADED.")
+        elan_vid_slicer.run()
     else:
         print("Failed to load or find an ELAN file and video to use. Please check directory and usage.")
         print("Run script with optional argument -h for help: e.g. python3 elan_video_slicer.py -h")
